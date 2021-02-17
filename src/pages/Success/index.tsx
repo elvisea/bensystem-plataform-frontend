e0da9d23ef65ae739f63a068404a37d29e1e31be
Elvis <elvis.e.amancio@gmail.com>
2021-02-17 20:46:39 -0300

    Ajustes

--- src/pages/Success/index.tsx
+++ src/pages/Success/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import './styles.css';
+
 const RegistrationSuccess: React.FC = () => {
   return (
-    <div id="page-registration-success">
-      <div className="success-content-wrapper">
+    <div id="page-landing">
+      <div id="page-landing-content" className="container">
         <main>
-          <h1>Ebaaa!</h1>
+          <h1>Tudo Certo!</h1>
 
           <p>
             Seu cadastro deu certo e foi enviado ao administrador para ser
